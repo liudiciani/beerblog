5800cddea42c3cdfcb93c26323a0e742f777b9de
Iudiciani <liudicia@LAMU02T65D8GTFM.uhc.com>
2017-07-11 16:48:46 -0400

    FIXED HOME STATE YAY

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ app.config([
 	'$stateProvider',
 	'$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider){
-		$stateProvider
-		.state('home', {
+		$stateProvider.state('home', {
 			url: '/home',
 			templateUrl: '/home.html',
 			controller: 'MainCtrl'
